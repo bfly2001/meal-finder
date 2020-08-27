2754c50593bc756bda204e147f0190cdcd736086
bfly2001 <brendan.sflynn@gmail.com>
2020-08-26 20:08:18 -0700

    Update script.js
    Clear single meal.
    
    function to search and fetch from api.

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ mealsEl = document.getElementById('meals'),
 resultHeading = document.getElementById('result-heading'),
 single_mealEl = document.getElementById('single-meal')
 
+//Search meal and fetch from API
+function searchMeal(e) {
+    e.preventDefault();
+}
+
+//Clear single meal
+single_mealEl.innerHTML = '';
+
+
 //Event Listeners
 submit.addEventListener('submit', searchMeal);
